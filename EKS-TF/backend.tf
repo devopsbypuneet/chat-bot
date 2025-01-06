e9e6698af014d0c36fc8592e27c75263e297ac23
Puneet <puneetp0705@gmail.com>
2025-01-06 15:32:23 +0530

    update vpc.tf and provider.tf backen.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "tetris-bucket"
-    region         = "us-east-1"
+    bucket         = "my-bucket-terraform-state"
+    region         = "ap-south-1"
     key            = "Chatbot-UI/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
@@ -9,7 +9,6 @@ terraform {
   required_version = ">=0.13.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
       source  = "hashicorp/aws"
     }
   }
